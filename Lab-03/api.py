1532306ee4c8909a080e63da87d0fde43db484dc
Tuan1112 <hongtuan1112@gmail.com>
2025-04-03 11:54:40 +0700

    Add files via upload

--- Lab-03/api.py
+++ Lab-03/api.py
@@ -1,14 +1,12 @@
 from flask import Flask, request, jsonify
 from cipher.rsa import RSACipher
-from cipher.ecc import ECCCipher
+from cipher.ecc import ECCipher
 
 app = Flask(__name__)
-
-# RSA CIPHER ALGORITHM
 rsa_cipher = RSACipher()
 
-@app.route('/api/rsa/generate_keys', methods=['GET'])
-def rsa_generate_keys():
+@app.route("/api/rsa/generate_keys", methods=["GET"])
+def generate_keys():
     rsa_cipher.generate_keys()
     return jsonify({'message': 'Keys generated successfully'})
 
@@ -16,92 +14,78 @@ def rsa_generate_keys():
 def rsa_encrypt():
     data = request.json
     message = data['message']
-    key_type = data['key_type']
-    
+    keytype = data['keytype']
     private_key, public_key = rsa_cipher.load_keys()
-    
-    if key_type == 'public':
+    if keytype == 'public':
         key = public_key
-    elif key_type == 'private':
+    elif keytype == 'private':
         key = private_key
     else:
         return jsonify({'error': 'Invalid key type'})
-    
     encrypted_message = rsa_cipher.encrypt(message, key)
     encrypted_hex = encrypted_message.hex()
-    
     return jsonify({'encrypted_message': encrypted_hex})
 
 @app.route("/api/rsa/decrypt", methods=["POST"])
 def rsa_decrypt():
     data = request.json
     ciphertext_hex = data['ciphertext']
-    key_type = data['key_type']
-    
+    keytype = data['keytype']
     private_key, public_key = rsa_cipher.load_keys()
-    
-    if key_type == 'public':
+    if keytype == 'public':
         key = public_key
-    elif key_type == 'private':
+    elif keytype == 'private':
         key = private_key
     else:
         return jsonify({'error': 'Invalid key type'})
-    
     ciphertext = bytes.fromhex(ciphertext_hex)
     decrypted_message = rsa_cipher.decrypt(ciphertext, key)
-    
     return jsonify({'decrypted_message': decrypted_message})
 
-@app.route('/api/rsa/sign', methods=['POST'])
-def rsa_sign_message():
+@app.route("/api/rsa/sign", methods=["POST"])
+def rsa_sign():
     data = request.json
     message = data['message']
-    
     private_key, _ = rsa_cipher.load_keys()
     signature = rsa_cipher.sign(message, private_key)
     signature_hex = signature.hex()
-    
     return jsonify({'signature': signature_hex})
 
-@app.route('/api/rsa/verify', methods=['POST'])
-def rsa_verify_signature():
+@app.route("/api/rsa/verify", methods=["POST"])
+def rsa_verify():
     data = request.json
     message = data['message']
     signature_hex = data['signature']
-    
-    public_key, _ = rsa_cipher.load_keys()
+    _, public_key = rsa_cipher.load_keys()
     signature = bytes.fromhex(signature_hex)
-    is_verified = rsa_cipher.verify(message, signature, public_key)
-    
-    return jsonify({'is_verified': is_verified})
+    is_verify = rsa_cipher.verify(message, signature, public_key)
+    return jsonify({'is_verify': is_verify})
 
-# ECC CIPHER ALGORITHM
-ecc_cipher = ECCCipher()
+ecc_cipher = ECCipher()
 
-@app.route('/api/ecc/generate_keys', methods=["GET"])
-def ecc_generate_keys():
+@app.route("/api/ecc/generate_keys", methods=["GET"])
+def generate_ecc_keys():  # Renamed function
     ecc_cipher.generate_keys()
     return jsonify({'message': 'Keys generated successfully'})
 
 @app.route("/api/ecc/sign", methods=["POST"])
-def ecc_sign_message():
+def ecc_sign():
     data = request.json
     message = data['message']
     private_key, _ = ecc_cipher.load_keys()
     signature = ecc_cipher.sign(message, private_key)
     signature_hex = signature.hex()
     return jsonify({'signature': signature_hex})
-
-@app.route('/api/ecc/verify', methods=['POST'])
-def ecc_verify_signature():
+    
+@app.route("/api/ecc/verify", methods=["POST"])
+def ecc_verify():
     data = request.json
     message = data['message']
     signature_hex = data['signature']
     _, public_key = ecc_cipher.load_keys()
     signature = bytes.fromhex(signature_hex)
-    is_verified = ecc_cipher.verify(message, signature, public_key)
-    return jsonify({'is_verified': is_verified})
+    is_verify = ecc_cipher.verify(message, signature, public_key)
+    return jsonify({'is_verify': is_verify})
 
-# Main function
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
